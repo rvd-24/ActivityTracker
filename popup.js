861c016ed1b7ef324daac076cc4cb2c2d5c02084
Raghav Vinayak Dadhich <72245900+rvd-24@users.noreply.github.com>
2022-06-30 15:10:25 +0530

    time details displayed on the extension

--- popup.js
+++ popup.js
@@ -7,13 +7,40 @@ function gotTabs(tabs){
     output=document.getElementById("tab");
     console.log(t);
 }
-
 var port = chrome.extension.connect({
     name: "Sample Communication"
 });
 var res=document.getElementById('res');
+var opentime=document.getElementById('opentime');
+var closetime=document.getElementById('closetime');
 port.postMessage("Hi BackGround");
 port.onMessage.addListener(function(msg) {
-    console.log("message recieved" + msg);
-    res.innerHTML+=msg;
+    var m=msg.split(')(*&^%$#@!');
+    var openm="";var closem="";
+    openm+=m[0];
+    closem+=m[1];
+    var tabsurl="";
+    openm=openm.split(';');
+    console.log(openm.length);
+    console.log(openm);
+    closem=closem.split(';');
+    console.log(closem.length);
+    console.log(closem);
+    var Tabsobj=JSON.parse(openm[1]);
+    for(i=0;i<Tabsobj.length;i++){
+        console.log(Tabsobj[i].url);
+        tabsurl+="<li>"+Tabsobj[i].url+"\n"+"</li>";
+    }
+    // console.log("message recieved" + msg);
+    res.innerHTML+=openm[0]+"<br>"+"Current Tabs"+"<br>"+tabsurl;
+    opentime.innerHTML=openm[openm.length-2]+"      "+"Tab Open Time: "+openm[openm.length-1];
+    closetime.innerHTML=closem[0]+"     "+closem[1]+"     "+closem[2];
 });
+// var m=msg.split(';');
+//     console.log(m.length);
+//     var tabsurl="";
+//     var Tabsobj=JSON.parse(m[1]);
+//     for(i=0;i<Tabsobj.length;i++){
+//         console.log(Tabsobj[i].url);
+//         tabsurl+="<li>"+Tabsobj[i].url+"\n"+"</li>";
+//     }

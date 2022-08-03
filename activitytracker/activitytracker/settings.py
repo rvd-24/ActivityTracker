58f69c85a3867aa65cba2b1018e5038cfe70c8c8
rvd-24 <raghavvinayak24@gmail.com>
2022-08-03 18:14:49 +0530

    secretkey update

--- activitytracker/activitytracker/settings.py
+++ activitytracker/activitytracker/settings.py
@@ -22,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ucf=ik@-tafh6n@y)ju*6p_r__(4*urnkh3+t6_xpd19!*c0_u'
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

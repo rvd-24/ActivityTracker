980241e509460a456904ccb1369587a468083f02
rvd-24 <raghavvinayak24@gmail.com>
2022-09-24 23:32:35 +0530

    tweaked app for heroku

--- activitytracker/activitytracker/settings.py
+++ activitytracker/activitytracker/settings.py
@@ -1,5 +1,8 @@
 import os
 from telnetlib import ENCRYPT
+import django_heroku
+import dj_database_url
+from decouple import config
 """
 Django settings for activitytracker project.
 
@@ -61,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'activitytracker.urls'
@@ -146,6 +150,8 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -153,3 +159,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())

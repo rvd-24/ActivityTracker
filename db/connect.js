6cf86557fb5962bc950cacd6fd787d8c59de89f2
Raghav Vinayak Dadhich <72245900+rvd-24@users.noreply.github.com>
2022-09-24 23:55:59 +0530

    Delete db directory

--- db/connect.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/TabDetails").then(()=>{
-    console.log("Connected successfully to MongoDB");  
-}).catch((err)=>{
-    console.log("Errors connecting to database");
-});
-var connect=mongoose.connection;

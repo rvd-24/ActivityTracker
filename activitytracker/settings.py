842eb4a050e2a3dc5d598cf063cffa523a418f60
rvd-24 <raghavvinayak24@gmail.com>
2022-09-25 13:15:26 +0530

    -

--- activitytracker/settings.py
+++ activitytracker/settings.py
@@ -148,11 +148,11 @@ ENCRYPT_KEY=b'43Q4wwu0ZIwEjxa74bomrTK4923pcw6IkkzlEX8tTfA='
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'staticfiles')
-]
+STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
